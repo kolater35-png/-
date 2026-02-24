c0676271251f350a9c9fec4172a636530bd7f441
kolater35-png <kolater35@gmail.com>
2026-02-24 12:24:05 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,33 @@
+from kivymd.app import MDApp
+from kivy.lang import Builder
+from kivy.clock import mainthread
+from core import TitanKernel
+from ui_modules import TitanIDE
+
+class TitanApp(MDApp):
+    def build(self):
+        self.theme_cls.theme_style = "Dark"
+        self.theme_cls.primary_palette = "Teal"
+        self.kernel = TitanKernel()
+        self.ide = TitanIDE()
+        return Builder.load_string(self.ide.get_layout())
+
+    def process(self):
+        """Запуск тяжелого анализа кода"""
+        code = self.root.ids.editor.text
+        self.root.ids.editor.text = self.ide.highlight(code)
+        self.kernel.heavy_operation(code, self.on_result)
+
+    @mainthread
+    def on_result(self, text):
+        from kivymd.uix.snackbar import Snackbar
+        Snackbar(text=text, bg_color=[0, 0.4, 0.4, 1]).open()
+
+    def pip_action(self):
+        """Быстрая установка зависимостей"""
+        res = self.kernel.install_module("requests")
+        self.on_result(res)
+
+if __name__ == "__main__":
+    TitanApp().run()
+  
